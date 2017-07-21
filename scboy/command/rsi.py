c506271939b55ac0b5cbf26ddcccbe09e773e7d6
Bowen <bowen.sui@hotmail.com>
2017-07-22 00:21:39 +0200

    use src_col_name instead of hard coding it to df['close']

--- scboy/command/rsi.py
+++ scboy/command/rsi.py
@@ -33,47 +33,10 @@ class Rsi:
     def help():
         parser.print_help()
 
-    def RSI(self, prices, n=14):
-        # https://stackoverflow.com/questions/20526414/relative-strength-index-in-python-pandas
-        # third answer
-        deltas = (prices['close'] - prices['close'].shift(1)).fillna(0)
-
-        # Calculate the straight average seed values.
-        # The first delta is always zero, so we will use a slice of the first n deltas starting at 1,
-        # and filter only deltas > 0 to get gains and deltas < 0 to get losses
-        avg_of_gains = deltas[1:n + 1][deltas > 0].sum() / n
-        avg_of_losses = -deltas[1:n + 1][deltas < 0].sum() / n
-
-        # Set up pd.Series container for RSI values
-        rsi_series = pd.Series(0.0, deltas.index)
-
-        # Now calculate RSI using the Wilder smoothing method, starting with n+1 delta.
-        up = lambda x: x if x > 0 else 0
-        down = lambda x: -x if x < 0 else 0
-        i = n + 1
-        for d in deltas[n + 1:]:
-            avg_of_gains = ((avg_of_gains * (n - 1)) + up(d)) / n
-            avg_of_losses = ((avg_of_losses * (n - 1)) + down(d)) / n
-            if avg_of_losses != 0:
-                rs = avg_of_gains / avg_of_losses
-                rsi_series[i] = 100 - (100 / (1 + rs))
-            else:
-                rsi_series[i] = 100
-            i += 1
-
-        return rsi_series
-
-    def talib_rsi(self, df, period):
-        return talib.RSI(df['close'].values, period)
-
-
     def execute(self) -> pd.DataFrame:
-        #self.df[self.col_name] = self.RSI(self.df, self.period).shift(1)
-
-        self.df[self.col_name] = self.talib_rsi(self.df, self.period)
-
+        prices = self.df[self.src_col_name].values
+        self.df[self.col_name] = talib.RSI(prices, self.period)
         return self.df
-
 
 if __name__ == '__main__':
     df = TestSeries(None, None).execute()
